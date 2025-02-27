c1bb382956e5c10fd9b1e3f67e25430d4f2fe9c9
Erik Mikac <5546184+HankSchrader@users.noreply.github.com>
2025-02-26 19:33:54 -0500

    changed githook name. Signed-off-by: Erik Mikac <5546184+HankSchrader@users.noreply.github.com>

---
+++ src/main/java/com/erik/githook_demo/GithookDemoApplication.java
@@ -0,0 +1,13 @@
+package com.erik.githook_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithookDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithookDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erik/githook_demo/GithookDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erik.githook_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithookDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
